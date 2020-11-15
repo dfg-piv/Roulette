815eafd85a08f75a93f3dd7d0680cfcfafe435e3
Isabelle <nema_i@hotmail.com>
2020-11-15 15:59:12 -0500

    dropdown added

--- load.php
+++ load.php
@@ -14,7 +14,16 @@ $c = $_POST['c'];
 ?>
 <div class="container-fluid">
  
-
+	<div id="topBar" class="row">
+		<div class="dropdown">
+		  <button id="favouritesButton" class="btn dropdown-toggle" type="button" data-toggle="dropdown">
+				Favourites
+		  	<span class="caret"></span>
+			</button>
+		  <ul id="favouritesDropdown" class="dropdown-menu">
+		  </ul>
+		</div>
+	</div>
   <div class="row logo">
     <div class="col-md-8 col-md-offset-2 col-sm-8 col-sm-offset-2 col-xs-12">
     
@@ -739,6 +748,21 @@ $(document).on('click', '.heart', (event) => {
 		$(target).attr("src","images/icons/heart-outline.png");
 	}
 });
+
+$(document).on('click', '#favouritesButton', () => {
+	if ($("#topBar .dropdown").hasClass("open")){
+		var favPoems = sessionStorage.getItem(favPoems);
+		if (!favPoems || favPoems === 0){
+			$("#favouritesDropdown").hide();
+		} else {
+			$("#favouritesDropdown").empty();
+			for (poem of favPoems){
+				$("#favouritesDropdown").append("<li><a href='" + poem.poemPath + "' target='_blank'>" + poem.title + " - " + poem.poet + "</a></li>");
+			}
+			$("#favouritesDropdown").show();
+		}
+	}
+})
 
 </script>
 
